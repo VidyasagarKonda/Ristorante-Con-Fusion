f65e91dcc3ba6561a04c2ba626b446fb5299188f
vidyasagar <vidyasagar.vsk77@gmail.com>
2023-02-12 12:45:05 +0530

    assignment 2

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -6,6 +6,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { COMMENTS } from '../Shared/comments';
 import { PROMOTIONS } from '../Shared/promotions';
 import { LEADERS } from '../Shared/leaders';
@@ -59,6 +60,11 @@ class Main extends Component {
           />
           <Route path='/menu/:dishId' component={DishWithId}></Route>
           <Route exact path='/contactus' component={Contact} />
+          <Route
+            exact
+            path='/aboutUs'
+            component={() => <About leaders={this.state.leaders} />}
+          />
           <Redirect to='/home' />
         </Switch>
         <Footer />
