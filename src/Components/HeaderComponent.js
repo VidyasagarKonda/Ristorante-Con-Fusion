5b3d9cea27a87dc22f02b8a65d81da63917f654a
vidyasagar <vidyasagar.vsk77@gmail.com>
2023-10-02 21:40:20 +0530

    logo path changed

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -55,7 +55,7 @@ class Header extends Component {
             <NavbarToggler onClick={this.toggleNav} />
             <NavbarBrand className='mr-auto' href='/'>
               <img
-                src='assets/images/logo.png'
+                src={require('../Shared/assets/images/logo.png')}
                 height='30'
                 width='41'
                 alt='Ristorante Con Fusion'
